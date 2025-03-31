06de9e638571304172028fe6a8e0aa96356c5b49
Regera94 <kamaugerald94@gmail.com>
2025-03-31 10:47:58 +0300

    added a custom exception when user already exists

--- src/main/java/com/gerald/accounts/service/AccountsService.java
+++ src/main/java/com/gerald/accounts/service/AccountsService.java
@@ -5,6 +5,5 @@ import com.gerald.accounts.dto.CustomerDto;
 public interface AccountsService {
 
       void createAccount(CustomerDto customerDto);
-      void updateAccount(CustomerDto customerDto, String customerId);
 
 }
